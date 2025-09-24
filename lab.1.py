11f8a3fcd57079c49297b08173ca7a0f9460bd61
ddan1rra <alipovatolganaj@gmail.com>
2025-09-24 14:51:04 +0600

    Create lab.1.py

---
+++ lab.1.py
@@ -0,0 +1,8 @@
+a, b = 17, 5
+print("a - b =", a - b)
+print("a + b =", a + b)
+print("a * b =", a * b)
+print("a * b =", a * b)
+print("a / b =", a / b)
+print("a // b =", a // b)
+print("a % b =", a % b)
